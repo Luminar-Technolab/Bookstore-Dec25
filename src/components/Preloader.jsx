3ecb3d65b85d47eda2d2e11c23e3f9dcb8911cb6
soniagkalliath <soniageorge111@gmail.com>
2026-04-07 11:33:21 +0530

    Design: Preloader, Header

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Preloader() {
+  return (
+    <div className='flex justify-center items-center w-full h-screen'>
+      <img src="https://cdnl.iconscout.com/lottie/premium/thumb/books-4895016-4127728.gif" alt="book" />
+    </div>
+  )
+}
+
+export default Preloader

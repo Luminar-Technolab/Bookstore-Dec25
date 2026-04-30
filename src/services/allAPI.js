14a97fd059d6d78391c5ef23fa0c07ff3f00ca58
soniagkalliath <soniageorge111@gmail.com>
2026-04-30 11:13:13 +0530

    ADD: book details

--- src/services/allAPI.js
+++ src/services/allAPI.js
@@ -19,3 +19,8 @@ export const googleLoginAPI = async (userData)=>{
 export const userUpdateAPI = async (userId,userData)=>{
     return await apiService("PUT",`/user/${userId}`,userData)
 }
+
+//addBook api : called by UploadBook compoenent when add btn clicked
+export const addBookAPI = async (bookDetails)=>{
+    return await apiService("POST",`/books`,bookDetails)
+}
